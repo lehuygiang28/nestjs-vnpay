e2b2fb2f55404be1bea7238cff68bba9487618ae
lehuygiang28 <lehuygiang28@gmail.com>
2024-08-06 15:56:03 +0700

    fix: :bug: make service available when import module

--- src/vnpay.module.ts
+++ src/vnpay.module.ts
@@ -2,6 +2,7 @@ import { type DynamicModule, Module } from '@nestjs/common';
 
 import type { VnpayModuleOptions } from './interfaces';
 import { VNPAY_MODULE_OPTIONS } from './vnpay.constant';
+import { VnpayService } from './vnpay.service';
 
 @Module({})
 // biome-ignore lint/complexity/noStaticOnlyClass: use for register module
@@ -9,7 +10,8 @@ export class VnpayModule {
     static register(options: VnpayModuleOptions): DynamicModule {
         return {
             module: VnpayModule,
-            providers: [{ provide: VNPAY_MODULE_OPTIONS, useValue: options || {} }],
+            providers: [{ provide: VNPAY_MODULE_OPTIONS, useValue: options || {} }, VnpayService],
+            exports: [VnpayService],
         };
     }
 }
