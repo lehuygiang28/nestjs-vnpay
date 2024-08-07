8c2585fd6a50169e0afb6dd80d90efaa6e4cda13
lehuygiang28 <lehuygiang28@gmail.com>
2024-08-07 18:35:54 +0700

    feat: :sparkles: add register async

--- src/interfaces/vnpay-module-option.interface.ts
+++ src/interfaces/vnpay-module-option.interface.ts
@@ -1,3 +1,16 @@
+import type { ModuleMetadata, Provider, Type } from '@nestjs/common';
 import type { VNPayConfig } from 'vnpay';
 
 export interface VnpayModuleOptions extends VNPayConfig {}
+
+export interface VnpayModuleOptionsFactory {
+    createVnpayOptions(): Promise<VnpayModuleOptions> | VnpayModuleOptions;
+}
+
+export interface VnpayModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    useExisting?: Type<VnpayModuleOptionsFactory>;
+    useClass?: Type<VnpayModuleOptionsFactory>;
+    useFactory?: (...args: any[]) => Promise<VnpayModuleOptions> | VnpayModuleOptions;
+    inject?: any[];
+    extraProviders?: Provider[];
+}
--- src/vnpay.module.ts
+++ src/vnpay.module.ts
@@ -1,6 +1,10 @@
-import { type DynamicModule, Module } from '@nestjs/common';
+import { type DynamicModule, Module, type Provider } from '@nestjs/common';
 
-import type { VnpayModuleOptions } from './interfaces';
+import type {
+    VnpayModuleAsyncOptions,
+    VnpayModuleOptions,
+    VnpayModuleOptionsFactory,
+} from './interfaces';
 import { VNPAY_MODULE_OPTIONS } from './vnpay.constant';
 import { VnpayService } from './vnpay.service';
 
@@ -11,6 +15,46 @@ export class VnpayModule {
             module: VnpayModule,
             providers: [{ provide: VNPAY_MODULE_OPTIONS, useValue: options || {} }, VnpayService],
             exports: [VnpayService],
+        };
+    }
+
+    static registerAsync(options: VnpayModuleAsyncOptions): DynamicModule {
+        return {
+            module: VnpayModule,
+            imports: options?.imports || [],
+            providers: [
+                ...VnpayModule.createAsyncProviders(options),
+                ...(options?.extraProviders ?? []),
+            ],
+        };
+    }
+
+    private static createAsyncProviders(options: VnpayModuleAsyncOptions): Provider[] {
+        if (options.useExisting || options.useFactory) {
+            return [VnpayModule.createAsyncOptionsProvider(options)];
+        }
+        return [
+            VnpayModule.createAsyncOptionsProvider(options),
+            {
+                provide: options?.useClass as any,
+                useClass: options?.useClass as any,
+            },
+        ];
+    }
+
+    private static createAsyncOptionsProvider(options: VnpayModuleAsyncOptions): Provider {
+        if (options?.useFactory) {
+            return {
+                provide: VNPAY_MODULE_OPTIONS,
+                useFactory: options.useFactory,
+                inject: options?.inject || [],
+            };
+        }
+        return {
+            provide: VNPAY_MODULE_OPTIONS,
+            useFactory: async (optionsFactory: VnpayModuleOptionsFactory) =>
+                optionsFactory.createVnpayOptions(),
+            inject: [options?.useExisting || options?.useClass] as any[],
         };
     }
 }
